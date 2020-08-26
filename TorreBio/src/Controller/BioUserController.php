aa00b11fa5ba571ac67b0acde61da938cc36381c
Segus <segus3088@gmail.com>
2020-08-26 05:52:51 -0500

    Consultando bio

--- TorreBio/src/Controller/BioUserController.php
+++ TorreBio/src/Controller/BioUserController.php
@@ -12,14 +12,18 @@ class BioUserController extends AbstractController
      */
     public function index()
     {
+    	//$bio = this->consultarBio();
+    	$bio = file_get_contents('https://bio.torre.co/api/bios/sebastianguachetasalazar');
         return $this->render('bio_user/index.html.twig', [
             'controller_name' => 'BioUserController',
             'name_user' => 'Sebastian Salazar',
-            'user_profile' => '',
+            'user_profile' => $bio,
         ]);
     }
 
     public function consultarBio($id){
     	//
+    	$bioJson = file_get_contents('https://bio.torre.co/api/bios/sebastianguachetasalazar');
+    	return $bioJson;
     }
 }
