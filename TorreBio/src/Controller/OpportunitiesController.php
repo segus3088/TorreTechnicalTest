ab3d7c74ae3f73952e55f7f16ed626d759a053c5
Segus <segus3088@gmail.com>
2020-08-28 17:20:16 -0500

    Ajuste para mostrar Oportunidad

--- TorreBio/src/Controller/OpportunitiesController.php
+++ TorreBio/src/Controller/OpportunitiesController.php
@@ -8,12 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class OpportunitiesController extends AbstractController
 {
     /**
-     * @Route("/opportunities", name="opportunities")
+     * @Route("/opportunities/{idOpp}", name="opportunities")
      */
-    public function index()
+    public function index($idOpp)
     {
+    	$opportunitie = file_get_contents("https://torre.co/api/opportunities/$idOpp");
+    	$oppData = json_decode($opportunitie, true);
         return $this->render('opportunities/index.html.twig', [
             'controller_name' => 'OpportunitiesController',
+            'title_opp' => $oppData["objective"]
         ]);
     }
 }
